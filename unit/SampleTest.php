ce6bbcd5c9cd268b9ec65442f675ea652d3dacc4
Hiroki Yoshida <h-yoshida@mediba.jp>
2014-07-08 17:04:18 +0900

    travis

---
+++ unit/SampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * SampleTest
+ *
+ * @category Test
+ * @package  Sample
+ * @author   Everyone <username@example.com>
+ * @license  Example http://example.com
+ * @link     none
+ */
+class SampleTest extends TestCase
+{
+
+    /**
+     * testAuth
+     *
+     * @return void
+     */
+    public function testAuth()
+    {
+        $this->assertEquals('1', '1');
+    }
+
+}
